ea342721da994474ea6e6e82ede3ef3dfae3c3e1
tertcoder <82041184+tertcoder@users.noreply.github.com>
2023-02-04 00:57:07 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,85 @@
+// Variables for buttons
+
+const startStopBtn = document.querySelector("#startStopBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+// Variables for time values
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+// Variable for leading zeros
+
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
+
+// Variables for set interval & timerstatus
+
+let timerInterval = null;
+let timerStatus = "stopped";
+
+// Stop watch function
+
+function stopWatch() {
+  seconds++;
+
+  if (seconds / 60 === 1) {
+    seconds = 0;
+    minutes++;
+
+    if (minutes / 60 === 1) {
+      minutes = 0;
+      hours++;
+    }
+  }
+
+  if (seconds < 10) {
+    leadingSeconds = "0" + seconds.toString();
+  } else {
+    leadingSeconds = seconds;
+  }
+  if (minutes < 10) {
+    leadingMinutes = "0" + minutes.toString();
+  } else {
+    leadingMinutes = minutes;
+  }
+  if (hours < 10) {
+    leadingHours = "0" + hours.toString();
+  } else {
+    leadingHours = hours;
+  }
+
+  let displayTimer = (document.getElementById("timer").innerText =
+    leadingHours + ":" + leadingMinutes + ":" + leadingSeconds);
+}
+
+//
+startStopBtn.addEventListener("click", function () {
+  if (timerStatus === "stopped") {
+    timerInterval = window.setInterval(stopWatch, 1000);
+    document.getElementById(
+      "startStopBtn"
+    ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff" id="pause">
+    <path fill-rule="evenodd" d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z" clip-rule="evenodd" />
+  </svg>
+  `;
+    timerStatus = "started";
+  } else {
+    window.clearInterval(timerInterval);
+    document.getElementById("startStopBtn").innerHTML = `<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="#fff"
+    id="play"
+  >
+    <path
+      fill-rule="evenodd"
+      d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
+      clip-rule="evenodd"
+    />
+  </svg>`;
+  }
+  timerStatus = "stopped";
+});
