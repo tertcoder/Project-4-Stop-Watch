6926435463d1a1b67a93d700a2bb7a5cec560e96
tertcoder <82041184+tertcoder@users.noreply.github.com>
2023-02-04 03:37:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -55,37 +55,21 @@ function stopWatch() {
     leadingHours + ":" + leadingMinutes + ":" + leadingSeconds);
 }
 
-// guma hamwe nd
 startStopBtn.addEventListener("click", function () {
-  //
-
   if (timerStatus === "stopped") {
     timerInterval = window.setInterval(stopWatch, 1000);
-    // startStopBtn.removeEventListener("click", () => {
-    //   timerStatus = "started";
-    // });
 
     document.getElementById(
       "startStopBtn"
     ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff" id="pause">
-    <path fill-rule="evenodd" d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z" clip-rule="evenodd" />
-  </svg>
-  `;
+    <path fill-rule="evenodd" d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z" clip-rule="evenodd" /></svg>`;
     timerStatus = "started";
   } else {
     window.clearInterval(timerInterval);
-    document.getElementById("startStopBtn").innerHTML = `<svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="#fff"
-    id="play"
-  >
-    <path
-      fill-rule="evenodd"
-      d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
-      clip-rule="evenodd"
-    />
-  </svg>`;
+    document.getElementById(
+      "startStopBtn"
+    ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24" fill="#fff" id="play"><path fill-rule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clip-rule="evenodd"/></svg>`;
     timerStatus = "stopped";
   }
 });
